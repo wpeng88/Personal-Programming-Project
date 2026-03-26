9aa57e7d09cc920863a181c07944726d7fca3196
William Peng <wpeng@connect.kellettschool.com>
2026-03-26 03:05:30 +0000

    Mar 26, 2026, 11:05 AM

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
 #test
+
+rules = ("""
+Starting: The first click is always safe, often revealing a blank area.
+Numbers: A number (1-8) indicates exactly how many mines are in the 8 adjacent cells - horizontal, vertical, and diagonal.
+Empty Cells: Choosing a cell with no adjacent mines reveals a large area
+Winning: Clear all non-mine cells.
+Losing: If you choose a cell with a mine.
+Flagging: type 'flag' before coordinate to mark cells you suspect are mines (eg. if you wanted to flag cell 'H2' you would say 'flag H2')""")
+
+for char in rules:
+    sys.stdout.write(char)
+    sys.stdout.flush() # Forces immediate display
+    time.sleep(0.02) # Delays for 0.1 seconds 
