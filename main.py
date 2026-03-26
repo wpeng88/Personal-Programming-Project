d670525fe6c08dd2294068c46018ba5e42197820
William Peng <wpeng@connect.kellettschool.com>
2026-03-26 02:43:05 +0000

    Mar 26, 2026, 10:43 AM

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 ## Personal Programming Project - William Peng
-# import time
-# from random import randint
+import os, time, random
+from random import randint
 
 def main():
     showrules()
     game()
+
+def clear_screen():
+    os.system
 
 def showrules():
     print("Starting: The first click is always safe, often revealing a blank area. \nNumbers: A number (1-8) indicates exactly how many mines are in the 8 adjacent cells - horizontal, vertical, and diagonal.\nEmpty Cells: Choosing a cell with no adjacent mines reveals a large area\nWinning: Clear all non-mine cells.\nLosing: If you choose a cell with a mine.\nFlagging: type 'flag' before coordinate to mark cells you suspect are mines (eg. if you wanted to flag cell 'H2' you would say 'flag H2')")
