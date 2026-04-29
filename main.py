a258b0c4f14da00e97d0f8dd0d8c70bba5eab1f8
William Peng <wpeng@connect.kellettschool.com>
2026-04-29 03:10:21 +0000

    29 Apr 2026, 11:10

--- main.py
+++ main.py
@@ -54,7 +54,7 @@ def set_grid():
              ["A8", "B8", "C8", "D8", "E8", "F8", "G8", "H8"]]
     return grid
 
-def display_grid(grid):
+def display_grid():
     rows = 8
     cols = 8
     column_headers = "A B C D E F G H"
@@ -63,15 +63,15 @@ def display_grid(grid):
         row_display = f"{row:<2}"
         row_display += "⬜️" * cols 
         print(row_display)
-
-    rows = len(grid)
-    cols = len(grid[0])
-    column_headers = " ".join([chr(65 + i) for i in range(cols)])
-    print("  " + column_headers) 
-    for row in range(rows):
-        row_display = f"{row + 1:<2}"
-        row_display += " ".join(grid[row])
-        print(row_display)
+# def display_grid(grid)
+    # rows = len(grid)
+    # cols = len(grid[0])
+    # column_headers = " ".join([chr(65 + i) for i in range(cols)])
+    # print("  " + column_headers) 
+    # for row in range(rows):
+    #     row_display = f"{row + 1:<2}"
+    #     row_display += " ".join(grid[row])
+    #     print(row_display)
 
 def random_bomb_placement(grid):
     chosen_bomb_cells = []
