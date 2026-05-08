bb07c7a7c3cfb75fcf4b3ab6e89f402810764f2d
William Peng <wpeng@connect.kellettschool.com>
2026-05-08 06:49:22 +0000

    8 May 2026, 14:49

--- main.py
+++ main.py
@@ -98,6 +98,7 @@ def random_bomb_placement(grid):
         x, y = int(cell[0]) - 1, int(cell[1]) - 1
         grid[x][y] = "💣"
     print(chosen_bomb_cells)
+    print(grid)
     return chosen_bomb_cells
     
 #   for
